5bb8870f03d95bf9812a22542afdc8ab80a6a0c0
DerekBreton1 <144245693+DerekBreton1@users.noreply.github.com>
2024-10-22 19:28:20 -0400

    Added app initialization and first routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,1 +1,12 @@
+from flask import render_template, request
+from app import app
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/query', methods=['POST'])
+def query():
+    # Process form data and return results
+    return render_template('index.html', results=2)#some_results)
 
