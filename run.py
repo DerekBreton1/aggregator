a9c8be80fa816901f84bf8a73550fbeac1b6729b
DerekBreton1 <144245693+DerekBreton1@users.noreply.github.com>
2024-10-23 13:21:12 -0400

    Fixed CORS typo and enabled CORS for all routes

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from app import app
 from app.routes import register_routes
+from flask_cors import CORS
 
 app = Flask(__name__)
+# Enable CORS for all routes
+CORS(app)
 
 # Register routes
 register_routes(app)
